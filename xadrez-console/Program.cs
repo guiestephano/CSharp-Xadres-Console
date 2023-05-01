e208383be85a552a64b81ebd6eea401fa16ccfcb
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2023-04-30 22:49:24 -0300

    Projeto criado

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace xadres_console{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Hello word!");
+        }
+    }
+    }
